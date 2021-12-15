5ebab630fc74d14937a3f3442cbac1aefb690900
JohannesNagele <johannes.nagele-extern@deutschebahn.com>
2021-12-15 13:40:44 +0100

    reviewer requests implemented

--- settings_template.py
+++ settings_template.py
@@ -3,6 +3,6 @@ from pathlib import Path
 # Dir where all heros' Optolith .json are located
 data_folder = Path("")
 # List of single heros' Optolith .json names
-heros = ["res/friedo_firlefanz.json",]
+heros = ["res/Friedo_Firlefanz.json", "res/Maaso.json"]
 # Boolean whether stats shall be shown when heros are loaded
 show_values = True
