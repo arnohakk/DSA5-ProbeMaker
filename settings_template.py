81665ea05ad6094f72acc8580e03d9539713ef5f
Johannes Nagele <Johannes.Nagele@alexanderthamm.com>
2022-01-06 18:47:44 +0100

    conf

--- settings_template.py
+++ settings_template.py
@@ -3,7 +3,7 @@ from pathlib import Path
 # Dir where all heros' Optolith .json are located
 data_folder = Path("")
 # List of single heros' Optolith .json names
-heros = ["res/Friedo_Firlefanz.json", "res/Maaso.json"]
+heros = ["res/Friedo Firlefanz.json",]
 # Boolean whether stats shall be shown when heros are loaded
 show_values = True
 debug = True
