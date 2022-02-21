93d593f5e9c76c06908e6fd537c2dd826192cf83
arnohakk <arnehartz@hotmail.com>
2022-02-21 19:33:15 +0100

    Added Baduin

--- settings_template.py
+++ settings_template.py
@@ -3,7 +3,7 @@ from pathlib import Path
 # Dir where all heros' Optolith .json are located
 data_folder = Path("")
 # List of single heros' Optolith .json names
-heros = ["res/Friedo Firlefanz.json", "res/Maaso.json", "res/Wanda Weinreb.json","res/Garonn Regentag.json"]
+heros = ["res/Friedo Firlefanz.json", "res/Maaso.json", "res/Wanda Weinreb.json","res/Garonn Regentag.json", "res/Baduin Nepolemo Kalman Falconieri.json"]
 
 debug_log = True  # Use separate log not to mess real data collection
 show_values = False  # Boolean whether stats shall be shown when heros are loaded
